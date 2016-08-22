4d659f1a5ee1f74108a0a45aaa3675ada150b13e
14budig <nbudig@gmail.com>
2016-08-22 11:03:40 -0700

    solves myFind

--- starter-code/myFind.js
+++ starter-code/myFind.js
@@ -1,7 +1,11 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Find
 function myFind(arr, callback) {
-
-//  CODE INSIDE HERE   //
+  for(var x = 0; x < arr.length; x++){
+    if(callback(arr[x], x, arr)){
+      return arr[x];
+    }
+  }
+return undefined;
 
 }
 
